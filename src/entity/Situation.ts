8c492064e9866bba3eb2ed900fd718a7b17f7998
Samyr Tertuliano <samyrtertuliano4@gmail.com>
2026-08-27 23:57:18 -0300

    Criação da entity e classes Situation e Users

---
+++ src/entity/Situation.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Users } from "./Users";
+
+@Entity("situations")
+export class Situation {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nameSituation!: string;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt!: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updatedAt!: Date;
+
+    @OneToMany(() => Users, (user) => user.situation)
+    users!: Users[]
+}
---
+++ src/entity/Users.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm"
+import { Situation } from "./Situation"
+
+@Entity("users")
+export class Users {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nome!: string;
+
+    @Column({ unique: true })
+    email!: string;
+
+    @ManyToOne(() => Situation, (situation) => situation.users)
+    @JoinColumn({ name: "situationId" })
+    situation!: Situation;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    createdAt!: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updatedAt!: Date;
+
+}
